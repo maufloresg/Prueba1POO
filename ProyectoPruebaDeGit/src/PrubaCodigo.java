c1572f02403d732e240d87b2932a1a8a53081efe
maufg <maufg@gmail.com>
2020-10-29 12:45:34 -0600

    Cambio numero dos

--- ProyectoPruebaDeGit/src/PrubaCodigo.java
+++ ProyectoPruebaDeGit/src/PrubaCodigo.java
@@ -3,6 +3,7 @@ public class PrubaCodigo {
 
 	public static void main(String[] args) {
 		System.out.println("Prueba de Git Eclipse");
+		System.out.println("Prueba de Git Eclipse Cambio1");
 	}
 
 }
