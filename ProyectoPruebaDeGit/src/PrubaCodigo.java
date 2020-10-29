3eae17a54013d5c2a14b5b05611553f92aec778b
maufg <maufg@gmail.com>
2020-10-29 13:12:04 -0600

    NuevaClase

--- ProyectoPruebaDeGit/src/PrubaCodigo.java
+++ ProyectoPruebaDeGit/src/PrubaCodigo.java
@@ -1,9 +1,16 @@
+import operaciones.operacionesBasicas;
+
 
 public class PrubaCodigo {
 
 	public static void main(String[] args) {
 		System.out.println("Prueba de Git Eclipse");
 		System.out.println("Prueba de Git Eclipse Cambio1");
+		operacionesBasicas obj1 = new operacionesBasicas(5,6);
+		
+		int resultado = obj1.sumaNumeros();
+		System.out.println("El resultado de la suma es: " + resultado);
+		
 	}
 
 }
---
+++ ProyectoPruebaDeGit/src/operaciones/operacionesBasicas.java
@@ -0,0 +1,17 @@
+package operaciones;
+
+public class operacionesBasicas {
+	
+	private int a;
+	private int b;
+	
+	public operacionesBasicas(int a, int b){
+	this.a = a;
+	this.b = b;
+	}
+	
+	public int sumaNumeros(){
+		return a+b;
+	}
+
+}
